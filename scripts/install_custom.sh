005a3bc59cb251b74acf830366bbff6910cfcd71
Tania Allard <taniar.allard@gmail.com>
2020-01-10 10:54:19 +0000

    Add custom script

---
+++ scripts/install_custom.sh
@@ -0,0 +1,7 @@
+!/bin/bash	
+
+# install JupyterHub
+curl https://raw.githubusercontent.com/jupyterhub/the-littlest-jupyterhub/master/bootstrap/bootstrap.py \
+| sudo python3 - --admin $1 \
+# install PANGEO plugin
+--plugin git+https://github.com/yuvipanda/tljh-pangeo@v0.1 >> /var/log/TLJH_install.log 2>&1
