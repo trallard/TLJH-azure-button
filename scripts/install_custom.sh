3a9586793c9aa320028cfb1f491109eaefd44c73
Tania Allard <taniar.allard@gmail.com>
2020-01-13 14:42:30 +0000

    :bug: Try with voila plugin

--- scripts/install_custom.sh
+++ scripts/install_custom.sh
@@ -1,5 +1,6 @@
 # !/bin/bash	
 
-# install JupyterHub
+#!/bin/bash
 curl https://raw.githubusercontent.com/jupyterhub/the-littlest-jupyterhub/master/bootstrap/bootstrap.py \
-| sudo python3 - --admin ${1} --plugin git+https://github.com/yuvipanda/tljh-pangeo@v0.1
+ | sudo python3 - \
+   --plugin git+https://github.com/voila-dashboards/tljh-voila-gallery@master#"egg=tljh-voila-gallery&subdirectory=tljh-voila-gallery"
